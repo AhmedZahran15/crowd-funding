a209436248a43ac736ff4543ab772a31b956451b
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-21 10:59:29 +0300

    Register Category and Tag models in Django admin for improved project management.

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, ProjectImage, Donation, Comment, Rating, ProjectReport, CommentReport
+from .models import Project, ProjectImage, Donation, Comment, Rating, ProjectReport, CommentReport, Category, Tag
 
 # Register your models here.
 
@@ -10,3 +10,5 @@ admin.site.register(Comment)
 admin.site.register(Rating)
 admin.site.register(ProjectReport)
 admin.site.register(CommentReport)
+admin.site.register(Category)
+admin.site.register(Tag)
