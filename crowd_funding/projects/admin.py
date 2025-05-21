8b4f34d23ef9976a9bb3b78962a5710b30509f62
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-21 09:58:37 +0300

    Register project-related models in Django admin for enhanced management interface.

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project, ProjectImage, Donation, Comment, Rating, ProjectReport, CommentReport
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(ProjectImage)
+admin.site.register(Donation)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(ProjectReport)
+admin.site.register(CommentReport)
