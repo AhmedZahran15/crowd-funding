b2eb09c0cbca8e3f7493d127de589b027216675f
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-21 09:58:01 +0300

    Add custom UserManager for user creation and validation, update User model to utilize the new manager, and standardize string quotes in the model fields.

--- crowd_funding/accounts/models.py
+++ crowd_funding/accounts/models.py
@@ -1,16 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     mobile_phone = models.CharField(
         max_length=11,
-        validators=[RegexValidator(regex=r'^01[0125][0-9]{8}$', message="Invalid Egyptian phone number")]
+        validators=[
+            RegexValidator(
+                regex=r"^01[0125][0-9]{8}$", message="Invalid Egyptian phone number"
+            )
+        ],
     )
-    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="profiles/", null=True, blank=True)
     birthdate = models.DateField(null=True, blank=True)
     facebook_profile = models.URLField(null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
@@ -18,8 +55,10 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
