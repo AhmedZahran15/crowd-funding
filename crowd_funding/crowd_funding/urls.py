d78de75c8337b00aa612914cd92c7906a79c6862
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-05-23 21:51:12 +0300

    Project_view fundraiser page

--- crowd_funding/crowd_funding/urls.py
+++ crowd_funding/crowd_funding/urls.py
@@ -19,10 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from projects.views import fundraiser_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('fundraiser/<int:project_id>/', views.fundraiser_view, name='fundraiser')
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- crowd_funding/projects/views.py
+++ crowd_funding/projects/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
+from datetime import datetime
+from .models import Project, Comment
+from django.utils import timezone
 
-# Create your views here.
+comments = [
+    {
+        'name': 'Alice Johnson',
+        'comment': 'So happy to see this goal reached. Truly inspiring!',
+        'time': datetime(2024, 3, 25, 14, 30).strftime('%B %d, %Y %H:%M')
+    },
+    {
+        'name': 'Mohamed Salah',
+        'comment': 'May God reward everyone who contributed ❤️',
+        'time': datetime(2024, 3, 25, 16, 45).strftime('%B %d, %Y %H:%M')
+    },
+    {
+        'name': 'Fatima Elbaz',
+        'comment': 'This is the power of community. Well done everyone!',
+        'time': datetime(2024, 3, 26, 9, 15).strftime('%B %d, %Y %H:%M')
+    },
+        {
+        'name': 'Alice Johnson',
+        'comment': 'So happy to see this goal reached. Truly inspiring!',
+        'time': datetime(2024, 3, 25, 14, 30).strftime('%B %d, %Y %H:%M')
+    },
+    {
+        'name': 'Mohamed Salah',
+        'comment': 'May God reward everyone who contributed ❤️',
+        'time': datetime(2024, 3, 25, 16, 45).strftime('%B %d, %Y %H:%M')
+    }
+]
+
+def fundraiser_view(request, project_id):
+
+
+    project = get_object_or_404(Project, pk=project_id)
+    # Handle comment submission
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        comment = request.POST.get('comment')
+        if name and comment:
+            Comment.objects.create(
+                project=project,
+                name=name,
+                comment=comment,
+                time=timezone.now()
+            )
+        return redirect('fundraiser', project_id=project.id)
+
+    # Fetch comments from the database by time
+    comments = project.comments.order_by('-time')
+    percent = int(min((project.raised / project.goal) * 100, 100))
+
+
+
+    context = {
+        'goal': project.goal,
+        'raised': project.raised,
+        'percent': percent,
+        'description': project.description,
+        'rating': project.rating,
+        'creator': {
+            'name': project.creator_name,
+            'image': project.creator_image,
+        },
+        'created_at': project.created_at,
+        'comments': comments,
+    }
+
+    return render(request, 'fundraiser.html', context)
