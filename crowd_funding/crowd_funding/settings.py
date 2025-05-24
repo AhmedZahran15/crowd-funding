c2a89ace2bcd49949bd4c7548f72d910b8fe84fc
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-24 09:28:04 +0300

    Add AuthRoutesMiddleware to the middleware stack for enhanced authentication handling

--- crowd_funding/crowd_funding/settings.py
+++ crowd_funding/crowd_funding/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "accounts.middleware.AuthRoutesMiddleware",
 ]
 
 ROOT_URLCONF = "crowd_funding.urls"
