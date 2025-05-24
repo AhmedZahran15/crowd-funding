8874338374ecfe20926dbda56521fad2f5459ef0
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-24 08:46:28 +0300

    Implement home view to display featured, latest, and highest-rated projects with categories

--- crowd_funding/crowd_funding/views.py
+++ crowd_funding/crowd_funding/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from projects.models import Project, Category
+from django.db.models import Count, Avg
 
 
 def home(request):
-    return render(request, "home.html")
-
+    featured_projects = Project.objects.filter(is_featured=True)[:5]
+    latest_projects = Project.objects.order_by("-start_time")[:5]
+    highest_rated_projects = (
+        Project.objects.annotate(avg_rating=Avg("rating__rating"))
+        .filter(avg_rating__isnull=False)
+        .order_by("-avg_rating")[:5]
+    )
+    if highest_rated_projects.count() < 5:
+        highest_rated_projects = Project.objects.annotate(
+            donation_count=Count("donation")
+        ).order_by("-donation_count")[:5]
+    categories = Category.objects.all()[:5]
+    context = {
+        "featured_projects": featured_projects,
+        "latest_projects": latest_projects,
+        "highest_rated_projects": highest_rated_projects,
+        "categories": categories,
+    }
+    return render(request, "home.html", context)
