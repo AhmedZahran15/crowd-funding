fbe906dbf3963becce603cc4703d177c26ba6e6d
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-24 16:15:30 +0300

    Remove unused imports from URL configuration

--- crowd_funding/crowd_funding/urls.py
+++ crowd_funding/crowd_funding/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from projects.views import project_view , donation_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
